24d179cb26317dc8d36d28ed60f51f3fc40cfc52
Bassil-Ali <baselali337@gmail.com>
2024-04-30 13:26:23 +0300

    navbar name

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -346,7 +346,7 @@
        
       </div>
       <div class="info">
-         <a href="" class="d-block">{{  it()->url(admin()->user()->name)}}
+         <a href="" class="d-block">{{  admin()->user()->name}}
         </a>
       </div>
     </div>
